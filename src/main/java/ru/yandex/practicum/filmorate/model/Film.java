a454bd665f8d85669f88413055465f31112c96db
ymolyavkin <mat2017u@yandex.ru>
2023-04-08 10:20:26 +0300

    feat : The program passes 61 tests, fails 6 tests.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -10,14 +10,15 @@ import org.springframework.cglib.core.Local;
 
 @Data
 public class Film {
+    private int i = 0;
     private final int id;
     private final String name;
     private final String description;
     private final LocalDate releaseDate;
     private final Duration duration;
 
-    public Film(int id, String name, String description, LocalDate releaseDate, Duration duration) {
-        this.id = id;
+    public Film(String name, String description, LocalDate releaseDate, Duration duration) {
+        id = ++i;
         this.name = name;
         this.description = description;
         this.releaseDate = releaseDate;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Setter;
 
@@ -7,15 +8,72 @@ import java.time.LocalDate;
 import java.util.Date;
 
 @Data
+//@Builder
+/*public class User {
+
+    private final int id;
+    private String name;
+    private final String email;
+    private final String login;
+    private final LocalDate birthday;
+    public static class Builder {
+        // Обязательные параметры
+
+        private final String email;
+        private final String login;
+
+
+        // Необязательные параметры с значениями по умолчанию
+
+        private int id = 0;
+        private String name = "";
+        private LocalDate birthday=LocalDate.now();
+
+        public Builder(String email, String login) {
+            this.email = email;
+            this.login = login;
+        }
+
+        public Builder id(int val) {
+            id = val;
+            return this;
+        }
+
+        public Builder name(String val) {
+            name = val;
+            return this;
+        }
+        public Builder birthday(LocalDate val) {
+            birthday = val;
+            return this;
+        }
+
+
+        public User build() {
+            return new User(this);
+        }
+    }
+
+
+    private User(Builder builder) {
+        id = builder.id;
+        name = builder.name;
+        email = builder.email;
+        login = builder.login;
+        birthday = builder.birthday;
+    }
+}*/
+
 public class User {
-    private int id = 1;
+    private int i = 0;
+    private final int id;
     private String name;
     private final String email;
     private final String login;
     private final LocalDate birthday;
 
     public User(String name, String email, String login, LocalDate birthday) {
-        id = id++;
+        id = ++i;
         this.name = name;
         this.email = email;
         this.login = login;
@@ -26,3 +84,4 @@ public class User {
         this.name = name;
     }
 }
+
