e80411b994ada2da05df7cc9b901bb0785b37614
ymolyavkin <mat2017u@yandex.ru>
2023-04-13 07:34:10 +0300

    style : I deleted unused import in ValidatorTest class.

--- src/test/java/ru/yandex/practicum/filmorate/controller/validation/ValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/validation/ValidatorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
