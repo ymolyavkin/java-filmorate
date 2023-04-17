035a8facf930c76e03ef7adaf89d94adf3d099c6
ymolyavkin <mat2017u@yandex.ru>
2023-04-17 14:48:17 +0300

    feat : I added the friends field to the User class.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Data
@@ -12,12 +14,14 @@ public class User {
     private final String email;
     private final String login;
     private final LocalDate birthday;
+    private Set<Long> friends;
 
     public User(String name, String email, String login, LocalDate birthday) {
         this.name = name;
         this.email = email;
         this.login = login;
         this.birthday = birthday;
+        friends=new HashSet<>();
     }
 }
 
