42e13566f80182ea7c216c519bb943bc6a56389d
ymolyavkin <mat2017u@yandex.ru>
2023-04-24 11:12:43 +0300

    style : I added empty line in UserStorage and FilmStorage interfaces.

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,8 +6,12 @@ import java.util.Map;
 
 public interface FilmStorage {
     public Map<Integer, Film> findAll();
+
     public void addFilm(Film film);
+
     public void updateFilm(Film film);
+
     public Film findFilmById(Integer filmId);
+
     public void deleteFilm(Film film);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,12 +1,17 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.Map;
 
 public interface UserStorage {
     public Map<Integer, User> findAll();
+
     public void addUser(User user);
+
     public void updateUser(User user);
+
     public User findUserById(Integer userId);
+
     public void deleteUser(User user);
 }
