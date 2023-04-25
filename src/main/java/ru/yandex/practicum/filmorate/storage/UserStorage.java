1b2e3d8f48dfc676d873771a2281811e50118012
ymolyavkin <mat2017u@yandex.ru>
2023-04-25 14:15:25 +0300

    style : I removed unused imports.

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
-import java.util.Map;
 
 public interface UserStorage {
     public List<User> findAll();
